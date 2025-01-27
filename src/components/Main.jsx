5d46342dfe6b275f03a141ab25fde212c2d817c6
Olga <minaeva9@gmail.com>
2025-01-27 16:52:17 -0500

    useState used

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,12 @@
+import { useState } from "react";
+
 function Main() {
   const [feedback, setFeedback] = useState(false);
 
   return (
     <div className="bg-purple-100 text-stone-900 p-4 h-screen flex flex-col justify-center items-center gap-2">
       <p className="text-2xl ">Please give us your feedback</p>
-      <button
-        className="bg-purple-600 text-stone-100 rounded py-2 px-4 text-"
-        onClick={() => alert("Thank you for your feedback!")}
-      >
+      <button className="bg-purple-600 text-stone-100 rounded py-2 px-4 text-">
         Feedback
       </button>
     </div>
